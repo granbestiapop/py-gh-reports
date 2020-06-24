38bb273fb36a06cc22841e24ef205ca38665e368
granbestiapop <leonardo.clavijo@mercadolibre.com>
2020-06-24 00:44:27 -0300

    Fix

--- service/milestone.py
+++ service/milestone.py
@@ -7,7 +7,6 @@ milestone_regex = re.compile(r"https://github.com/(.*)/(.*)/milestone/(\d*)")
 def process_data(data):
   return [transform_repository(Dict(data['data'][key])) for key in data['data']]
 
-
 def transform_repository(repository):
   milestone = repository.milestone
   pull_requests = [pr for pr in milestone.pullRequests.nodes]
@@ -30,7 +29,7 @@ def extract_url_info(url):
   application = m.group(2)
   milestone_id = m.group(3)
   q_id = hashlib.md5(f"{application}{milestone_id}".encode('utf')).hexdigest()
-  return {'org': org, 'application':application, 'milestone_id': milestone_id, 'q_id': q_id}
+  return {'org': org, 'application':application, 'milestone_id': milestone_id, 'q_id': "m_"+q_id}
 
 def milestone_info(urls):
   urls = re.split(',|\n', urls)
